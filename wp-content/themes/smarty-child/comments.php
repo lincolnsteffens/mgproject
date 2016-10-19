d8ec5a28a1c860caaae45547ed3e570ea0df045d
Sushank Jadhav <sushank.jadhav.prdxn@gmail.com>
2016-10-19 17:34:40 +0530

    Created child theme

---
+++ wp-content/themes/smarty-child/comments.php
@@ -0,0 +1,3 @@
+<?php
+get_template_part( 'parts/'.smarty_get_university_mode().'/comments', get_post_format() );
+?>
---
+++ wp-content/themes/smarty-child/search.php
@@ -0,0 +1,3 @@
+<?php
+get_template_part( 'parts/'.smarty_get_university_mode().'/search', get_post_format() );
+?>
